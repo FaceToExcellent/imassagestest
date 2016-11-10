b1dd8fb83532bffad9deecedc028506b682fa899
huamanlou <15810427253@163.com>
2016-11-10 11:18:03 +0800

    Initial Commit

---
+++ MessagesExtension/MessagesViewController.h
@@ -0,0 +1,13 @@
+//
+//  MessagesViewController.h
+//  MessagesExtension
+//
+//  Created by HYZ on 16/11/10.
+//  Copyright © 2016年 HYZ. All rights reserved.
+//
+
+#import <Messages/Messages.h>
+
+@interface MessagesViewController : MSMessagesAppViewController
+
+@end
